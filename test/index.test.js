48c810ffd4cae808942c8186747ff8bcf6eebd38
Peter <peter@praekelt.com>
2018-06-01 21:06:28 +0200

    testing CI

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,12 @@
-const { expect } = require('chai')
-const {doSomething} = require('../index')
+const { expect } = require('chai');
+const { doSomething } = require('../index');
 
 describe('dummy test', () => {
-
   it('should add 2', () => {
-    expect(doSomething(2)).to.be.equal(4)
-  })
-})
+    expect(doSomething(2)).to.be.equal(4);
+  });
+
+  it('should not add 3', () => {
+    expect(doSomething(2)).to.not.equal(5);
+  });
+});
